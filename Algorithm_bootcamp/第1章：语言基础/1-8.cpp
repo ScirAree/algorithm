c275985fece1b626d364947f4a710a3d37ab06d8
ScirAree <whr_zm@163.com>
2023-04-16 20:56:27 +0800

    提交

--- Algorithm_bootcamp/第1章：语言基础/1-8.cpp
+++ Algorithm_bootcamp/第1章：语言基础/1-8.cpp
@@ -1,3 +1,15 @@
 //输入一个整数，判断其是否是水仙花数。水仙花数是指一个3位数，它的各位数字的3次幂之和等于它本身
 #include<iostream>
 using namespace std;
+int main(){
+    int num,a,b,c;
+    cin>>num;
+    a=num/100;
+    b=(num/10)%10;
+    c=num%10;
+    if(num==a*a*a+b*b*b+c*c*c)
+        cout<<"是水仙花数"<<endl;
+    else
+        cout<<"不是"<<endl;
+    return 0;
+}
